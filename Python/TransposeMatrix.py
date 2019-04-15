efcfecbc5075285c41c9d2eb5b7d87a73d45699c
Winterdirge <sunshine9062@gmail.com>
2019-04-15 15:43:51 +0800

    add transpose matrix

---
+++ Python/TransposeMatrix.py
@@ -0,0 +1,20 @@
+# 给定一个矩阵 A， 返回 A 的转置矩阵。
+class Solution(object):
+    def transpose(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not A:
+            return None
+        result = []
+        for j in range(len(A[0])):
+            temp = []
+            for i in range(len(A)):
+                temp.append(A[i][j])
+            result.append(temp)
+        return result
+
+    def transpose1(self, A):
+        return [[A[i][j] for i in range(len(A))] for j in range(len(A[0]))]
+print Solution().transpose1([[1,2,3],[4,5,6],[7,8,9]])
