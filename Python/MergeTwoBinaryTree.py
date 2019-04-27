909d9ca0a356b727e4a0e666949f5c015362505f
Winterdirge <sunshine9062@gmail.com>
2019-04-27 22:04:22 +0800

    add merge two binary tree

---
+++ Python/MergeTwoBinaryTree.py
@@ -0,0 +1,17 @@
+# 合并二叉树
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
