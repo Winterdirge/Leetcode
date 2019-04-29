6e7a2cb891736b809dad657a4f7e57aa7c7e6cdf
Winterdirge <sunshine9062@gmail.com>
2019-04-30 00:23:22 +0800

    add minimum depth binary tree.

---
+++ Python/MinimumDepthofBinaryTree.py
@@ -0,0 +1,13 @@
+# 111. Minimum Depth of Binary Tree
+class Solution(object):
+    def minDepth(self, root):
+        if not root:
+            return 0
+        if root.left and root.right:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+        elif root.left:
+            return 1 + self.minDepth(root.left)
+        elif root.right:
+            return 1 + self.minDepth(root.right)
+        else:
+            return 1
