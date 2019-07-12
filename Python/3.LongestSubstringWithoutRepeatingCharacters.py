f77c4c4fe53f8b905f016f68ad56549dd9483c67
Winterdirge <sunshine9062@gmail.com>
2019-07-12 16:14:40 +0800

    add 3.longest_substring_without_repeating_character.

---
+++ Python/3.LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,13 @@
+"""
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+"""
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        dic = {}
+        i, res = 0, 0
+        for j in range(len(s)):
+            if s[j] in dic:
+                i = max(i, dic[s[j]])
+            res = max(res, j - i + 1)
+            dic[s[j]] = j + 1
+        return res
