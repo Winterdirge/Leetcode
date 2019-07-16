725981985b0035b3f3621a514e234507ae117e3a
Winterdirge <sunshine9062@gmail.com>
2019-07-16 16:20:05 +0800

    add '55.Jump Game' solution.

---
+++ Python/55.JumpGame.py
@@ -0,0 +1,13 @@
+"""
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个位置。
+"""
+def canJump(nums):
+    tmp = len(nums) - 1
+    for i in range(len(nums)-1, -1, -1):
+        if i + nums[i] >= tmp:
+            tmp = i
+    return tmp == 0
