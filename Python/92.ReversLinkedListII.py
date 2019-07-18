edc058e3b80dbf34987d84340e669d368ce006ef
Winterdirge <sunshine9062@gmail.com>
2019-07-18 11:58:13 +0800

    92. reverse linked list ii.

---
+++ Python/92.ReversLinkedListII.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+"""
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+
+示例:
+
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        p = dummy
+        for i in xrange(1, m):
+        	p = p.next
+        head = p.next
+        for i in range(m, n):
+        	tmp = head.next
+        	head.next = tmp.next
+        	tmp.next = p.next
+        	p.next = tmp
+        return dummy.next
